1ee616e3207bf2b428d2c9e71c6c09c8dd4d15ac
stratacast <courthicks1@gmail.com>
2020-08-08 20:33:58 -0700

    Additions to accounts code

--- lib/accounts.ex
+++ lib/accounts.ex
@@ -31,6 +31,28 @@ defmodule NodePing.Accounts do
   end
 
   @doc """
+  Get information about your NodePing account or subaccount
+
+  ## Parameters
+
+  - `token` - NodePing API token that is provided with account
+  - `customerid` - optional ID to access a subaccount
+
+  ## Examples
+
+      iex> token = System.fetch_env!("TOKEN")
+      iex> result = NodePing.Accounts.get_account!(token)
+      iex> is_map(result)
+      true
+  """
+  def get_account!(token, customerid \\ nil) do
+    case get_account(token, customerid) do
+      {:ok, result} -> result
+      {:error, error} -> error
+    end
+  end
+
+  @doc """
   Create a subaccount for your NodePing account
 
   ## Parameters
@@ -44,7 +66,7 @@ defmodule NodePing.Accounts do
 
   ## Examples
 
-  iex> token = System.fetch_env!("TOKEN")
+      iex> token = System.fetch_env!("TOKEN")
       iex> args = %{:contactname => "My Name", :email => "me@example.com", :timezone => "-7", :location => "name"}
       iex> {:ok, result} = NodePing.Accounts.create_subaccount(token, args)
       iex> is_map(result)
@@ -55,6 +77,33 @@ defmodule NodePing.Accounts do
 
     combine_map_struct(NodePing.Accounts.SubAccount, args)
     |> (&post(@api_url <> "/accounts" <> querystrings, &1)).()
+  end
+
+  @doc """
+  Create a subaccount for your NodePing account
+
+  ## Parameters
+
+  - `token` - NodePing API token that is provided with account
+  - `args` - A map of arguments for creating the subaccount
+
+  The `NodePing.Accounts.SubAccount` can be used to create the map necessary
+  for the `args` variable and the variables expected by the API to create a
+  new subaccount.
+
+  ## Examples
+
+      iex> token = System.fetch_env!("TOKEN")
+      iex> args = %{:contactname => "My Name", :email => "me@example.com", :timezone => "-7", :location => "name"}
+      iex> {:ok, result} = NodePing.Accounts.create_subaccount!(token, args)
+      iex> is_map(result)
+      true
+  """
+  def create_subaccount!(token, args) do
+    case create_subaccount(token, args) do
+      {:ok, result} -> result
+      {:error, error} -> error
+    end
   end
 
   @doc """
@@ -72,7 +121,7 @@ defmodule NodePing.Accounts do
 
   ## Examples
 
-  iex> token = System.fetch_env!("TOKEN")
+      iex> token = System.fetch_env!("TOKEN")
       iex> args = %{:name => "Contact Name", :email => "me2@example.com", :timezone => "-2"}
       iex> {:ok, result} = NodePing.Accounts.update_account(token, args)
       iex> result["timezone"] == "-2.0"
@@ -90,7 +139,51 @@ defmodule NodePing.Accounts do
     |> (&put(@api_url <> "/accounts" <> querystrings, &1)).()
   end
 
-  def delete_subaccount(token, customerid) when is_bitstring(customerid) do
+  @doc """
+  Update your NodePing account or subaccount details
+
+  ## Parameters
+
+  - `token` - NodePing API token that is provided with account
+  - `args` - A map of arguments for creating the subaccount
+  - `customerid` - An optional subaccount ID that will be updated instead of the main account
+
+  The `NodePing.Accounts.SubAccount` can be used to create the map necessary
+  for the `args` variable and the variables expected by the API to create a
+  new subaccount.
+
+  ## Examples
+
+      iex> token = System.fetch_env!("TOKEN")
+      iex> args = %{:name => "Contact Name", :email => "me2@example.com", :timezone => "-2"}
+      iex> {:ok, result} = NodePing.Accounts.update_account!(token, args)
+      iex> result["timezone"] == "-2.0"
+      true
+  """
+  def update_account!(token, args, customerid \\ nil) do
+    case update_account(token, args, customerid) do
+      {:ok, result} -> result
+      {:error, error} -> error
+    end
+  end
+
+  @doc """
+  Delete your NodePing subaccount
+
+  ## Parameters
+
+  - `token` - NodePing API token that is provided with account
+  - `customerid` - The subaccount ID for the subaccount you want to delete
+
+  ## Examples
+
+      iex> token = System.fetch_env!("TOKEN")
+      iex> subaccount = System.fetch_env!("SUBACCOUNT")
+      iex> {:ok, result} = NodePing.Accounts.delete_subaccount!(token, subaccount)
+      iex> is_map(result)
+      true
+  """
+  def delete_subaccount(token, customerid) do
     querystrings =
       add_cust_id([{:token, token}], customerid)
       |> merge_querystrings()
@@ -98,11 +191,72 @@ defmodule NodePing.Accounts do
     delete(@api_url <> "/accounts" <> querystrings)
   end
 
+  @doc """
+  Delete your NodePing subaccount
+
+  ## Parameters
+
+  - `token` - NodePing API token that is provided with account
+  - `customerid` - The subaccount ID for the subaccount you want to delete
+
+  ## Examples
+
+      iex> token = System.fetch_env!("TOKEN")
+      iex> subaccount = System.fetch_env!("SUBACCOUNT")
+      iex> {:ok, result} = NodePing.Accounts.delete_subaccount!(token, subaccount)
+      iex> is_map(result)
+      true
+  """
+  def delete_subaccount!(token, customerid) do
+    case delete_subaccount(token, customerid) do
+      {:ok, result} -> result
+      {:error, error} -> error
+    end
+  end
+
+  @doc """
+  Disable notifications on your account or subaccount
+
+  ## Parameters
+
+  - `token` - NodePing API token that is provided with account
+  - `accountsupressall` - boolean value. true disables notifications, false enables them
+  - `customerid` - optional customerid if you want to disable notifications on a subaccount
+
+  ## Examples
+
+      iex> token = System.fetch_env!("TOKEN")
+      iex> subaccount = System.fetch_env!("SUBACCOUNT")
+      iex> {:ok, result} = NodePing.Accounts.disable_notifications(token, true, subaccount)
+  """
   def disable_notifications(token, accountsuppressall, customerid \\ nil)
       when is_boolean(accountsuppressall) do
     [{:token, token}, {:accountsuppressall, accountsuppressall}]
     |> add_cust_id(customerid)
     |> merge_querystrings()
     |> (&put(@api_url <> "/accounts" <> &1)).()
+  end
+
+  @doc """
+  Disable notifications on your account or subaccount
+
+  ## Parameters
+
+  - `token` - NodePing API token that is provided with account
+  - `accountsupressall` - boolean value. true disables notifications, false enables them
+  - `customerid` - optional customerid if you want to disable notifications on a subaccount
+
+  ## Examples
+
+      iex> token = System.fetch_env!("TOKEN")
+      iex> subaccount = System.fetch_env!("SUBACCOUNT")
+      iex> {:ok, result} = NodePing.Accounts.disable_notifications!(token, true, subaccount)
+  """
+  def disable_notifications!(token, accountsuppressall, customerid \\ nil)
+      when is_boolean(accountsuppressall) do
+    case disable_notifications(token, accountsuppressall, customerid) do
+      {:ok, result} -> result
+      {:error, error} -> error
+    end
   end
 end

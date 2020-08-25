3f09840b2e8ea21160f9a370cea7e051d5615a63
stratacast <courthicks1@gmail.com>
2020-08-24 21:22:51 -0700

    Version bump to 0.9.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NodePing.MixProject do
   def project do
     [
       app: :nodeping,
-      version: "0.9.1",
+      version: "0.9.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()

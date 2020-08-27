1ba0c6896fdfba4378000646f1d996703a215fb0
stratacast <courthicks1@gmail.com>
2020-08-26 20:22:43 -0700

    Updated to version 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NodePing.MixProject do
   def project do
     [
       app: :nodeping,
-      version: "0.9.2",
+      version: "1.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()

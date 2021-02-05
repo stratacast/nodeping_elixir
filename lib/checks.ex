b79b8b1f925d9c4c99e27c50cb658a3c1641d65f
stratacast <courthicks1@gmail.com>
2021-02-04 18:03:21 -0800

    Fix for disable_all_checks

--- lib/checks.ex
+++ lib/checks.ex
@@ -578,7 +578,7 @@ defmodule NodePing.Checks do
   """
   def disable_all_checks(token, disableall, opts \\ []) do
     querystrings =
-      Helpers.add_cust_id([{:token, token}] ++ opts ++ [{:disableall, disableall}])
+      opts ++ [{:token, token}, {:disableall, disableall}]
       |> Helpers.merge_querystrings()
       |> URI.encode()
 

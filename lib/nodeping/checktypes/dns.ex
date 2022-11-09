2c3d7ba00e190dd28ed8af5136dbbd45dd14390a
stratacast <courthicks1@gmail.com>
2022-11-09 11:59:07 -0800

    Added missing threshold field and typespec

--- lib/nodeping/checktypes/dns.ex
+++ lib/nodeping/checktypes/dns.ex
@@ -19,8 +19,36 @@ defmodule NodePing.Checktypes.Dns do
     public: false,
     runlocations: nil,
     homeloc: false,
+    threshold: 5,
     sens: 2,
     dep: nil,
     notifications: []
   ]
+
+  @typedoc """
+  DNS Check field types
+  """
+  @type t :: %__MODULE__{
+    target: String.t(),
+    label: String.t() | nil,
+    type: String.t(),
+    port: non_neg_integer(),
+    autodiag: boolean(),
+    transport: String.t(),
+    dnstype: String.t(),
+    dnsrd: non_neg_integer(),
+    verify: boolean(),
+    dnssection: String.t(),
+    contentstring: nil | String.t(),
+    dnstoresolve: nil | String.t(),
+    interval: non_neg_integer(),
+    enabled: boolean(),
+    public: boolean(),
+    runlocations: nil | [String.t()],
+    homeloc: nil | String.t(),
+    threshold: non_neg_integer(),
+    sens: non_neg_integer(),
+    dep: nil | String.t(),
+    notifications: [map()]
+  }
 end

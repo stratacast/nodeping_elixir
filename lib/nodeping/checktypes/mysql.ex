efd194ce92f704043f35279008dd40622639547f
stratacast <courthicks1@gmail.com>
2022-11-09 16:02:20 -0800

    Update MYSQL check type

--- lib/nodeping/checktypes/mysql.ex
+++ lib/nodeping/checktypes/mysql.ex
@@ -7,6 +7,14 @@ defmodule NodePing.Checktypes.Mysql do
     label: "",
     autodiag: false,
     type: "MYSQL",
+    port: 3306,
+    username: nil,
+    password: nil,
+    secure: false,
+    ipv6: false,
+    database: nil,
+    query: nil,
+    fields: %{},
     interval: 15,
     enabled: false,
     public: false,
@@ -17,4 +25,31 @@ defmodule NodePing.Checktypes.Mysql do
     dep: nil,
     notifications: []
   ]
+
+  @typedoc """
+  MYSQL Check field types
+  """
+  @type t :: %__MODULE__{
+    target: String.t(),
+    label: String.t() | nil,
+    autodiag: boolean(),
+    type: String.t(),
+    port: non_neg_integer(),
+    username: String.t() | nil,
+    password: String.t() | nil,
+    secure: false | :ssl,
+    ipv6: boolean(),
+    database: nil | String.t(),
+    query: String.t(),
+    fields: map(),
+    interval: non_neg_integer(),
+    enabled: boolean(),
+    public: boolean(),
+    runlocations: nil | [String.t()],
+    homeloc: nil | String.t(),
+    threshold: non_neg_integer(),
+    sens: non_neg_integer(),
+    dep: nil | String.t(),
+    notifications: [map()]
+  }
 end

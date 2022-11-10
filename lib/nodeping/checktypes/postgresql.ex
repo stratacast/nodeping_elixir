68447b3f6083590544ac61cc11a40e40d9fb863a
stratacast <courthicks1@gmail.com>
2022-11-09 16:02:34 -0800

    Add new PostgreSQL check

---
+++ lib/nodeping/checktypes/postgresql.ex
@@ -0,0 +1,43 @@
+defmodule NodePing.Checktypes.Postgresql do
+  @moduledoc false
+
+  @enforce_keys [:target]
+  defstruct [
+    :target,
+    label: "",
+    autodiag: false,
+    type: "PGSQL",
+    query: nil,
+    fields: %{},
+    interval: 15,
+    enabled: false,
+    public: false,
+    runlocations: nil,
+    homeloc: false,
+    threshold: 5,
+    sens: 2,
+    dep: nil,
+    notifications: []
+  ]
+
+  @typedoc """
+  PostgreSQL Check field types
+  """
+  @type t :: %__MODULE__{
+    target: String.t(),
+    label: String.t() | nil,
+    autodiag: boolean(),
+    type: String.t(),
+    query: String.t(),
+    fields: map(),
+    interval: non_neg_integer(),
+    enabled: boolean(),
+    public: boolean(),
+    runlocations: nil | [String.t()],
+    homeloc: nil | String.t(),
+    threshold: non_neg_integer(),
+    sens: non_neg_integer(),
+    dep: nil | String.t(),
+    notifications: [map()]
+  }
+end
